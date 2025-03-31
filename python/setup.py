214451fc4a985f4c4e5d87b4677b72552fbfcc16
zvictor <zvictor@users.noreply.github.com>
2025-03-31 23:30:17 +0200

    .

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='brainyflow',
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['brainyflow'],
     author="Victor Duarte",
     description="Minimalist AI framework in 65 Lines. Enable LLMs to Program Themselves.",
