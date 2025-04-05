db7fa2aaebbea04d2958662585bd4a8fa27e4ae5
Dheerapat Tookkane <dheerapat38@gmail.com>
2025-04-05 22:02:15 +0700

    nit: change default question

--- cookbook/typescript-agent/main.ts
+++ cookbook/typescript-agent/main.ts
@@ -2,7 +2,7 @@ import { createAgentFlow } from "./flow";
 import { SearchAgentSharedContext } from "./nodes";
 
 async function main() {
-  const question = "what is tao bin?";
+  const question = "what is agentic ai?";
   const agentFlow = createAgentFlow();
   const sharedContext: SearchAgentSharedContext = {
     question: question,
