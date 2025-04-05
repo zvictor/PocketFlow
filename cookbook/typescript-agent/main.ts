2b8bbe0f10670e8ae8033d261f1714267770905c
Dheerapat Tookkane <dheerapat38@gmail.com>
2025-04-05 22:16:42 +0700

    nit: formatter

--- cookbook/typescript-agent/main.ts
+++ cookbook/typescript-agent/main.ts
@@ -2,15 +2,15 @@ import { createAgentFlow } from "./flow";
 import { SearchAgentSharedContext } from "./nodes";
 
 async function main() {
-  const question = "what is agentic ai?";
-  const agentFlow = createAgentFlow();
-  const sharedContext: SearchAgentSharedContext = {
-    question: question,
-  }
-  await agentFlow.run(sharedContext);
+    const question = "what is agentic ai?";
+    const agentFlow = createAgentFlow();
+    const sharedContext: SearchAgentSharedContext = {
+        question: question,
+    }
+    await agentFlow.run(sharedContext);
 }
 
 main().catch((error) => {
-  console.error("Error running the agent:", error);
+    console.error("Error running the agent:", error);
 }
 );
